29ed8d4b9a202dc6e4e10cc69ff803577c808b73
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2025-12-16 23:44:08 -0300

    Add home spec

---
+++ client/cypress/e2e/home-spec.cy.js
@@ -0,0 +1,171 @@
+describe('home page', () => {
+    beforeEach(() => {
+        cy.createHero();
+        cy.visit('/');
+    });
+
+    afterEach(() => {
+        cy.get('@newHero').then((newHero) => {
+            cy.deleteHero(newHero.id);
+        });
+    });
+
+    describe('when user is not logged in', () => {
+        it('clicking on like should alert the user they need to login', () => {
+            cy.get('@newHero').then((newHero) => {
+                cy.contains('[data-cy=hero-card]', newHero.name)
+                .find('button[data-cy=like]')
+                .click();
+                cy.contains('You must log in to like.');
+                cy.get('button').contains('Ok').click();
+                cy.contains('You must log in to like.').should('not.exist');
+            });
+        });
+
+        it('clicking on hire should alert the user they need to login', () => {
+            cy.get('@newHero').then((newHero) => {
+                cy.contains('[data-cy=hero-card]', newHero.name)
+                .find('button[data-cy=money]')
+                .click();
+                cy.contains('You must log in to hire this hero.');
+                cy.get('button').contains('Ok').click();
+                cy.contains('You must log in to hire this hero.').should('not.exist');
+            });
+        });
+    });
+
+    describe('when normal user is logged in', () => {
+        beforeEach(() => {
+            cy.login('test@test.com', 'test123');
+            cy.visit('/');
+        });
+
+        it('clicking like on a hero should increase their fan count', function () {
+            cy.get('@newHero').then((newHero) => {
+                cy.contains('[data-cy=hero-card]', newHero.name)
+                .as('firstHero')
+                .find('[data-cy=fans]')
+                .as('fanSpan')
+                .then((el) => {
+                    cy.wrap(el.text()).as('fanCount');
+                });
+                cy.get('@firstHero').find('[data-cy=like]').click();
+                cy.get('@fanCount').then((fanCount) => {
+                cy.get('@fanSpan').should('have.text', Number(fanCount) + 1);
+                });
+            });
+        });
+
+        it('user should be able to hire a hero', function () {
+            cy.get('@newHero').then((newHero) => {
+                cy.contains('[data-cy=hero-card]', newHero.name)
+                .as('firstHero')
+                .find('[data-cy=saves]')
+                .as('saveSpan')
+                .then((el) => {
+                    cy.wrap(el.text()).as('saveCount');
+                });
+                cy.get('@firstHero').find('[data-cy=money]').click();
+                cy.get('button').contains('Yes').click();
+                cy.get('@saveCount').then((saveCount) => {
+                cy.get('@saveSpan').should('have.text', Number(saveCount) + 1);
+                });
+            });
+        });
+
+        it('user should be able to decline hiring a hero', function () {
+            cy.get('@newHero').then((newHero) => {
+                cy.contains('[data-cy=hero-card]', newHero.name)
+                .as('firstHero')
+                .find('[data-cy=saves]')
+                .as('saveSpan')
+                .then((el) => {
+                    cy.wrap(el.text()).as('saveCount');
+                });
+                cy.get('@firstHero').find('[data-cy=money]').click();
+                cy.get('button').contains('No').click();
+                cy.get('@saveCount').then((saveCount) => {
+                cy.get('@saveSpan').should('have.text', Number(saveCount));
+                });
+            });
+        });
+    });
+
+    describe('when admin user is logged in', () => {
+        beforeEach(() => {
+            cy.login('admin@test.com', 'test123');
+            cy.visit('/');
+        });
+
+        it('clicking like on a hero should increase their fan count', function () {
+            cy.get('@newHero').then((newHero) => {
+                cy.contains('[data-cy=hero-card]', newHero.name)
+                .as('firstHero')
+                .find('[data-cy=fans]')
+                .as('fanSpan')
+                .then((el) => {
+                    cy.wrap(el.text()).as('fanCount');
+                });
+                cy.get('@firstHero').find('[data-cy=like]').click();
+                cy.get('@fanCount').then((fanCount) => {
+                cy.get('@fanSpan').should('have.text', Number(fanCount) + 1);
+                });
+            });
+        });
+
+        it('admin should be able to hire a hero', function () {
+            cy.get('@newHero').then((newHero) => {
+                cy.contains('[data-cy=hero-card]', newHero.name)
+                .as('firstHero')
+                .find('[data-cy=saves]')
+                .as('saveSpan')
+                .then((el) => {
+                    cy.wrap(el.text()).as('saveCount');
+                });
+                cy.get('@firstHero').find('[data-cy=money]').click();
+                cy.get('button').contains('Yes').click();
+                cy.get('@saveCount').then((saveCount) => {
+                cy.get('@saveSpan').should('have.text', Number(saveCount) + 1);
+                });
+            });
+        });
+
+        it('clicking edit on a hero should redirect to edit page', function () {
+            cy.get('@newHero').then((newHero) => {
+                cy.contains('[data-cy=hero-card]', newHero.name)
+                .find('[data-cy=pencil]')
+                .click();
+                cy.location('pathname').should('equal', `/heroes/${newHero.id}/edit`);
+            });
+        });
+
+        it('admin should be able to delete a hero', function () {
+            cy.get('@newHero').then((newHero) => {
+                cy.contains('[data-cy=hero-card]', newHero.name)
+                .as('firstHero')
+                .then((el) => {
+                    cy.wrap(el)
+                    .find('[data-cy=name]')
+                    .invoke('text')
+                    .then((text) => cy.wrap(text).as('heroName'));
+                });
+                cy.get('@firstHero').find('[data-cy=trash]').click();
+                cy.contains('Delete Hero?');
+                cy.get('button').contains('Yes').click();
+                cy.get('@heroName').then((name) => {
+                cy.getByCy('hero-card').contains(name).should('not.exist');
+                });
+            });
+        });
+
+        it('admin should be able to decline deleting a hero', function () {
+            cy.get('@newHero').then((newHero) => {
+                cy.contains('[data-cy=hero-card]', newHero.name)
+                .find('[data-cy=trash]')
+                .click();
+                cy.contains('Delete Hero?');
+                cy.get('button').contains('No').click();
+            });
+        });
+  });
+});
